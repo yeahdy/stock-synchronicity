2de5512e081e53d15e6e8535a7587046c6188d37
yeahdy <yejine98@naver.com>
2024-11-04 13:12:38 +0900

    Refactor: StockService 추상화

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -1,30 +1,5 @@
 package com.example.stock.service;
 
-import com.example.stock.domain.Stock;
-import com.example.stock.repository.StockRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-@Service
-@RequiredArgsConstructor
-public class StockService {
-
-    private final StockRepository stockRepository;
-
-    /**
-     * @Transactional 을 사용해서 비동기처리를 할 경우 트랜잭션이 끝나는 시점에 다른 쓰레드가 메소드에 접근할 수 있게 된다.
-     * 따라서 DB에 갱신되기 전에 다른 쓰레드가 같은 재고를 조회하게 되면 재고 갱신이 올바르게 처리가 되지 않게 된다.
-     * 즉, 스프링의 트렌젝션은 별도의 쓰레드간의 트랜잭션 전파가 이뤄지지 않기 때문에 트랜잭션이 유지되지 않는다.
-     * @param id
-     * @param quantity
-     */
-    //@Transactional
-    public synchronized void decreaseStock(Long id, Long quantity) {
-        Stock stock = stockRepository.findById(id).orElseThrow();
-        stock.decrease(quantity);
-
-        stockRepository.saveAndFlush(stock);
-    }
-
+public interface StockService {
+    void decreaseStock(Long id, Long quantity);
 }
--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -12,11 +12,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class StockServiceTest {
 
+    @Qualifier("synchronizedStockService")
     @Autowired
     private StockService stockService;
 
